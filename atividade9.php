e6e2fa19b58b892277e231f0c39f34ef5f1b8eaa
Gabriel <gabriel_v_alves@estudante.sesisenai.org.br>
2025-07-15 11:48:48 -0300

    atividade9

---
+++ atividade9.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio 9</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <label for="numero">Receber dois números e exibir a soma de todos os números entre eles:</label>
+        <input type="number" id="numero" name="numero" required>
+          <input type="number" id="numero" name="numero2" required>
+        <button type="submit" name="verificar">Verificar</button>
+   </form>
+   
+    <?php
+   
+    ?>
+</body>
+</html>
